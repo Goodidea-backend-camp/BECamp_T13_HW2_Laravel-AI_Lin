6fa5673d88febed12ab10d10d5b4217fa5b2f823
linhn0617 <yyo833782@gmail.com>
2024-11-01 15:34:33 +0800

    feat: add ApiResponse trait for standardized API responses

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+    public function success($message,$statusCode = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+        ], $statusCode);
+        
+    }
+
+    public function error($message,$statuscode = 400)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+        ], $statuscode);
+    }
+}
