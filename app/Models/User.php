7ecfe73405558c8807528a6f4940c658edc3ec0c
linhn0617 <yyo833782@gmail.com>
2024-11-11 19:03:58 +0800

    style: apply code formatting via laravel pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable implements MustVerifyEmail
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+
     /**
      * The attributes that should be hidden for serialization.
      *
