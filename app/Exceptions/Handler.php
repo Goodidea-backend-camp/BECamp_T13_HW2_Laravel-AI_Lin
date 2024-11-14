1e2f27cc80faf3d7b376d321dc2ece03f68e51d8
linhn0617 <yyo833782@gmail.com>
2024-11-14 23:00:21 +0800

    style: apply code formatting via laravel pint

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,7 +5,6 @@ namespace App\Exceptions;
 use App\Traits\ApiResponse;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
@@ -20,10 +19,11 @@ class Handler extends \Illuminate\Foundation\Exceptions\Handler
             return $this->error('File not found', Response::HTTP_NOT_FOUND);
         } elseif ($e instanceof AuthorizationException) {
             return $this->error($e->getMessage(), Response::HTTP_FORBIDDEN);
-        }else if ($e instanceof ValidationException) {
-            $errorMessages = array_map(fn($messages) => implode(' ', $messages), $e->errors());
+        } elseif ($e instanceof ValidationException) {
+            $errorMessages = array_map(fn ($messages) => implode(' ', $messages), $e->errors());
             $formattedErrorMessage = implode(' ', $errorMessages);
-            return $this->error($formattedErrorMessage, Response::HTTP_UNPROCESSABLE_ENTITY);;
+
+            return $this->error($formattedErrorMessage, Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
         return $this->error($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
