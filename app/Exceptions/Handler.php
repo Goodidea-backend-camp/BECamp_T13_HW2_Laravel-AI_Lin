45595854bd420539d919db4beb27eec024da2fec
linhn0617 <yyo833782@gmail.com>
2024-11-14 16:38:21 +0800

    feat: create custom handler for standardized exception responses

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Traits\ApiResponse;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class Handler extends \Illuminate\Foundation\Exceptions\Handler
+{
+    use ApiResponse;
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return $this->error('File not found', Response::HTTP_NOT_FOUND);
+        } elseif ($e instanceof AuthorizationException) {
+            return $this->error($e->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+
+        return $this->error($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
