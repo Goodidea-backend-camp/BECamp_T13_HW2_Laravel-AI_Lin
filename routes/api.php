4f8e03dabe3fa1b1a3b9b044c50419014cd116bb
linhn0617 <yyo833782@gmail.com>
2024-11-15 17:17:59 +0800

    feat:create routes to handle threads

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GoogleAuthController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ThreadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,3 +27,15 @@ Route::middleware('auth:sanctum')->post('/google/setup', [GoogleAuthController::
 Route::middleware('auth:sanctum')->get('/users/{user}', [UserController::class, 'show']);
 //編輯使用者資訊
 Route::middleware('auth:sanctum')->patch('/users/{user}', [UserController::class, 'update']);
+
+//使用者建立新的對話串
+Route::middleware('auth:sanctum')->post('/threads', [ThreadController::class, 'store']);
+
+//使用者瀏覽自己創建的所有對話串（只有對話串名稱及種類）
+Route::middleware('auth:sanctum')->get('/threads', [ThreadController::class, 'index']);
+
+//使用者編輯對話串名稱
+Route::middleware('auth:sanctum')->patch('/threads/{thread}', [ThreadController::class, 'update']);
+
+//使用者刪除對話串
+Route::middleware('auth:sanctum')->delete('/threads/{thread}', [ThreadController::class, 'destroy']);
