e0b83fe9ad65619a64173479e179c36c37e692c9
linhn0617 <yyo833782@gmail.com>
2024-11-16 17:01:13 +0800

    feat:create Resource、Service and controller to manage threads

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateThreadRequest;
+use App\Http\Requests\UpdateThreadRequest;
+use App\Http\Resources\ThreadResource;
+use App\Models\Thread;
+use App\Services\ThreadService;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ThreadController extends Controller
+{
+    use ApiResponse;
+
+    public function __construct(
+        private ThreadService $ThreadService
+    ) {
+    }
+
+    //使用者可以查看自己的所有 Thread（只包括對話串名稱以及類型）
+    public function index(): ResourceCollection
+    {
+        $user = auth()->user();
+        $result = $this->ThreadService->getAllThreads($user);
+
+        return ThreadResource::collection($result);
+    }
+
+    //使用者建立對話串
+    public function store(CreateThreadRequest $request): ThreadResource|JsonResponse
+    {
+        $validatedData = $request->validated();
+        $user = auth()->user();
+
+        //將請求資料透過 ThreadService 進行處理
+        $result = $this->ThreadService->createThread($user, $validatedData);
+
+        return $result['status'] === 'error'
+            ? $this->error($result['message'], $result['statusCode'])
+            : new ThreadResource($result);
+    }
+
+    //使用者更新對話串名稱
+    public function update(UpdateThreadRequest $request, Thread $thread)
+    {
+        $validatedData = $request->validated();
+        $result = $this->ThreadService->updateThreadTitle($thread, $validatedData);
+
+        return new ThreadResource($result);
+    }
+
+    //使用者刪除對話串
+    public function destroy(Thread $thread): JsonResponse
+    {
+        $result = $this->ThreadService->deleteThread($thread);
+
+        return $this->success($result['message'], $result['statusCode']);
+    }
+}
---
+++ app/Services/ThreadService.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Thread;
+use App\Models\User;
+use App\Traits\ServiceResponse;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Redis;
+use Symfony\Component\HttpFoundation\Response;
+
+class ThreadService
+{
+    use AuthorizesRequests;
+    use ServiceResponse;
+
+    //免費用戶最大對話串數量
+    public const MAX_FREE_THREADS = 3;
+
+    // 建立新對話串
+    public function createThread(User $user, array $data): Thread|array
+    {
+        //確認使用者是否為付費用戶
+        if (! $user->is_premium) {
+            //非付費用戶則檢查對話串數量
+            $totalThreadCount = $this->getTotalThreadCount($user->id);
+            if ($totalThreadCount >= self::MAX_FREE_THREADS) {
+                return $this->formatResponse('error', 'You have reached the maximum limit of 3 threads as a free user.Please upgrade to premium to create more threads.', Response::HTTP_FORBIDDEN);
+            }
+        }
+        //將對話儲存至資料庫
+        $thread = $this->storeNewThread($user, $data);
+
+        //增加免費用戶對話串數量
+        if (! $user->is_premium) {
+            $this->incrementTotalThreadCount($user->id);
+        }
+
+        return $thread;
+
+    }
+
+    //取得對話串數量
+    private function getTotalThreadCount(int $userId): int
+    {
+        // 取得儲存在redis中的總對話串數量，如果沒有則回傳0
+        $count = (int) Redis::get(sprintf('user:%d:total_thread_count', $userId)) ?? 0;
+
+        return $count;
+    }
+
+    private function incrementTotalThreadCount(int $userId): void
+    {
+        Redis::incr(sprintf('user:%d:total_thread_count', $userId));
+    }
+
+    //將對話串儲存至資料庫
+    private function storeNewThread(User $user, array $data): Thread
+    {
+        $thread = new Thread();
+        $thread->user_id = $user->id;
+        $thread->title = $data['title'];
+        $thread->type = $data['type'];
+        $thread->save();
+
+        return $thread;
+    }
+
+    //查看該使用者建立的所有對話串名稱
+    public function getAllThreads(User $user)
+    {
+        $threads = Thread::where('user_id', $user->id)->get();
+
+        return $threads;
+    }
+
+    //使用者更新對話串名稱
+    public function updateThreadTitle(Thread $thread, array $data): Thread
+    {
+        $this->authorize('update', $thread);
+        $thread->title = $data['title'];
+        $thread->save();
+
+        return $thread;
+    }
+
+    public function deleteThread(Thread $thread): array
+    {
+        $this->authorize('delete', $thread);
+        $thread->delete();
+
+        if (! $thread->user->is_premium) {
+            $this->decrementTotalThreadCount($thread->user_id);
+        }
+
+        return $this->formatResponse('success', 'Thread deleted successfully', Response::HTTP_NO_CONTENT);
+
+    }
+
+    // 免費用戶在刪除對話時要同時刪除redis中的對話串數量
+    private function decrementTotalThreadCount(int $userId): void
+    {
+        Redis::decr(sprintf('user:%d:total_thread_count', $userId));
+    }
+}
