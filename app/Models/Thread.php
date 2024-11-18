a0515bca92b8f3febe550524ddc01e220d9c2801
linhn0617 <yyo833782@gmail.com>
2024-11-18 18:21:06 +0800

    feat:create message model and build relationship with thread model

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property int $user_id
@@ -23,5 +24,10 @@ class Thread extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
     }
 }
